fdf3b28abd0f1cd0809f7abefec35cbc44deb1e7
Anton Hansson <hansson@google.com>
2018-11-22 15:40:23 +0000

    whyred: Move system props file to /product
    This will change the props to be merged in /product/build.prop,
    instead of the current /system/build.prop
    
    Bug: 119911662
    Test: boot
    Test: diff getprop before & after

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,7 +173,7 @@ TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM hardware
