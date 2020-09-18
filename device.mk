f4e25eca99d5d16b76b1e94568b75ea4e2c44bcf
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    whyred: Build libhidltransport/libhwbinder
    * Needed for old blobs

--- device.mk
+++ device.mk
@@ -291,6 +291,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0
 
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
 # HW crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
