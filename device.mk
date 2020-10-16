5554d89610f8bce797a59d1d5f6d7a24aa61b794
Pig <pig.priv@gmail.com>
2020-10-17 04:21:13 +0800

    whyred: Build vendor variant of com.dsi.ant

--- device.mk
+++ device.mk
@@ -145,7 +145,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant.antradio_library \
-    com.dsi.ant@1.0
+    com.dsi.ant@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
