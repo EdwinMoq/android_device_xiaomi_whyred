c7b955e8ec335d16529a30ea0866cf2907d252a0
Edwin Moquete <edwinmmoquete@gmail.com>
2021-09-05 17:51:00 -0400

    whyred: Kang thermal config from ginkgo
     * From MIUI V12.5.1.0.RCOCNXM
     * Remove unused sensors
     * Unify into a single file

--- device.mk
+++ device.mk
@@ -431,9 +431,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(DEVICE_PATH)/configs/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
-    $(DEVICE_PATH)/configs/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf
+    $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Trust
 PRODUCT_PACKAGES += \
