745445967ea1818403a3130858a8f6382c2a7c91
Edwin Moquete <edwinmmoquete@gmail.com>
2021-09-05 17:41:47 -0400

    whyred: Update FM blobs from ginkgo

--- device.mk
+++ device.mk
@@ -241,8 +241,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FM2 \
     libqcomfm_jni \
-    qcom.fmradio \
-    qcom.fmradio.xml
+    qcom.fmradio
 
 PRODUCT_BOOT_JARS += \
     qcom.fmradio
